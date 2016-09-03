4cda5022dc9d22c7ba956a9c959ebd8f715b0b8c
usmaks <anoly.konz.0099@gmail.com>
2016-09-04 00:39:27 +0900

    Initial commitment

---
+++ server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
+
+//app.use(middleware.requireAuthentication);
+
+//app.get('/', function(req, res){
+//	res.send('Hello Express!');
+//});
+
+app.get('/about', middleware.requireAuthentication, function(req, res){
+	res.send('About Us!');
+});
+
+app.use(express.static(__dirname + '/public')); 
+//console.log(__dirname);
+
+app.listen(PORT, function (){
+	console.log('Express server started on port ' + PORT + '!');
+});
